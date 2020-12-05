ca794f309b05e5aa47ab7aa7f15316637dfae14e
valooford <vladan.vip.vladan@gmail.com>
2020-12-05 23:59:26 +0300

    Template HTML file and table constructing was added

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,9 +1,26 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-import styles from './index.css';
+// import styles from "./index.css";
 
-console.log('Hello OntoNet!');
+console.log("Hello OntoNet!");
 
-$('<div>Hello OntoNet!</div>')
-  .appendTo(document.documentElement)
-  .addClass(styles.text);
+// const $table = $("#table");
+
+const colNames = ["placeId", "tokenId", "values"];
+
+const data = [
+  [":p1", ":token1", "a,b"],
+  [":p2", ":token2", "a"],
+];
+
+// $("<th><td>Hello OntoNet!</th></tr>").appendTo($table);
+
+$("#table")
+  .append(
+    `<thead><tr>${colNames.map((cn) => `<th>${cn}</th>`).join()}</tr></thead>`
+  )
+  .append(
+    `<tbody>${data
+      .map((row) => `<tr>${row.map((d) => `<td>${d}</td>`).join()}</tr>`)
+      .join()}</tbody>`
+  );
--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const ForkTsChecherWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const ForkTsChecherWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // entry
   resolve: {
-    extensions: ['.ts', '.js', '.json'], // the default extensions list (.js must be included)
+    extensions: [".ts", ".js", ".json"], // the default extensions list (.js must be included)
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         options: {
           transpileOnly: true, // turn off type checking as it's slow
         },
@@ -22,16 +22,18 @@ module.exports = {
   plugins: [
     new ForkTsChecherWebpackPlugin({
       eslint: {
-        files: './src/**/*.ts', // files which are being linted
+        files: "./src/**/*.ts", // files which are being linted
       },
     }), // enable type checking in a separate process
-    new HtmlWebpackPlugin({ title: 'OntoNet' }), // generates index.html file
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }), // generates index.html file from template file
   ],
   optimization: {
-    runtimeChunk: 'single', // split runtime code into separate chunk
+    runtimeChunk: "single", // split runtime code into separate chunk
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     // filename
   },
 };
